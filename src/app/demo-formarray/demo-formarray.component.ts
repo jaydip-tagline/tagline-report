e4328a249dea56b2c9d09f39b199aa6c6b30f184
jaydiph-tagline <jaydiph.tagline@gmail.com>
2023-01-24 12:58:36 +0530

    Display Content through User Interection

--- src/app/demo-formarray/demo-formarray.component.ts
+++ src/app/demo-formarray/demo-formarray.component.ts
@@ -19,14 +19,39 @@ export class DemoFormarrayComponent implements OnInit {
   public taskDataError!: string;
   public progressDataError!: string;
   public pendingDataError!: string;
+  public queryError!: string;
+  public noteError!: string;
   public isInvalid!: boolean;
+  public monthList: string[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  fullDate: any = new Date();
+  date: string = this.fullDate.getDate();
+  month: string = this.monthList[this.fullDate.getMonth()];
+  year: string = this.fullDate.getFullYear();
+
   constructor(private fb: FormBuilder) {
+    // console.log('this.month :>> ', this.month);
     this.userReport = this.fb.group({
       clientName: [''],
       projectName: [''],
+      name: [''],
       taskData: this.fb.array([new FormControl('', [Validators.required])]),
       progressData: this.fb.array([new FormControl('', [Validators.required])]),
       pendingData: this.fb.array([new FormControl('', [Validators.required])]),
+      queries: this.fb.array([new FormControl('', [Validators.required])]),
+      notes: this.fb.array([new FormControl('', [Validators.required])]),
     });
   }
 
@@ -47,32 +72,60 @@ export class DemoFormarrayComponent implements OnInit {
     return this.userReport.get('pendingData') as FormArray;
   }
 
-  addTaskdata() {
-    console.log('formControls:>> ', this.taskData.value);
-    if (this.formControls['taskData'].valid) {
-      this.taskData.push(this.formControl());
-      this.isInvalid = false;
-    } else {
-      this.taskDataError = this.errorMsg;
-      this.isInvalid = true;
-    }
+  get queries() {
+    return this.userReport.get('queries') as FormArray;
   }
-  addProgresstask() {
-    if (this.formControls['progressData'].valid) {
-      this.progressData.push(this.formControl());
-      this.isInvalid = false;
-    } else {
-      this.progressDataError = this.errorMsg;
-      this.isInvalid = true;
-    }
+  get notes() {
+    return this.userReport.get('notes') as FormArray;
   }
-  addPendingtask() {
-    if (this.formControls['pendingData'].valid) {
-      this.pendingData.push(this.formControl());
-      this.isInvalid = false;
-    } else {
-      this.pendingDataError = this.errorMsg;
-      this.isInvalid = true;
+
+  addData(arrayName: string) {
+    console.log(arrayName);
+    switch (arrayName) {
+      case 'taskData':
+        if (this.formControls['taskData'].valid) {
+          this.taskData.push(this.formControl());
+          this.isInvalid = false;
+        } else {
+          this.taskDataError = this.errorMsg;
+          this.isInvalid = true;
+        }
+        break;
+      case 'progressData':
+        if (this.formControls['progressData'].valid) {
+          this.progressData.push(this.formControl());
+          this.isInvalid = false;
+        } else {
+          this.progressDataError = this.errorMsg;
+          this.isInvalid = true;
+        }
+        break;
+      case 'pendingData':
+        if (this.formControls['pendingData'].valid) {
+          this.pendingData.push(this.formControl());
+          this.isInvalid = false;
+        } else {
+          this.pendingDataError = this.errorMsg;
+          this.isInvalid = true;
+        }
+        break;
+      case 'queries':
+        if (this.formControls['queries'].valid) {
+          this.queries.push(this.formControl());
+          this.isInvalid = false;
+        } else {
+          this.queryError = this.errorMsg;
+          this.isInvalid = true;
+        }
+        break;
+      case 'notes':
+        if (this.formControls['notes'].valid) {
+          this.notes.push(this.formControl());
+          this.isInvalid = false;
+        } else {
+          this.noteError = this.errorMsg;
+          this.isInvalid = true;
+        }
     }
   }
 
