8a06d0235b705b7abf41585f4cfb58601e8b27e6
jaydiph-tagline <jaydiph.tagline@gmail.com>
2023-01-26 18:33:11 +0530

    Implement post & delete Httpsclient method

--- src/app/http-client/http-client.component.ts
+++ src/app/http-client/http-client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../common';
 import { MainService } from '../main.service';
 
 @Component({
@@ -7,14 +9,47 @@ import { MainService } from '../main.service';
   styleUrls: ['./http-client.component.scss'],
 })
 export class HttpClientComponent implements OnInit {
-  constructor(private mainService: MainService) {
-    this.mainService.postUsers().subscribe((resp) => {
-      console.log(resp);
+  public users!: any;
+  public form!: FormGroup;
+  public formData: any;
+  public isUpdate: boolean = false;
+  public userId!: number;
+  constructor(private mainService: MainService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [null, Validators.required],
+      phone: [null, Validators.required],
+      email: [null, Validators.required],
     });
     this.mainService.getUsers().subscribe((resp) => {
-      console.log(resp);
+      this.users = resp;
     });
   }
 
   ngOnInit(): void {}
+
+  get formControls() {
+    return this.form.controls;
+  }
+
+  onSave() {
+    this.mainService.postUsers(this.form.value).subscribe((resp: any) => {
+      console.log(resp);
+      const data = {
+        id: this.users.length + 1,
+        ...this.form.value,
+      };
+      this.users.push(data);
+    });
+  }
+
+  edit(data: any) {
+    this.form.patchValue(data);
+    this.userId = data.id;
+    this.isUpdate = true;
+  }
+  delete(i: number) {
+    this.mainService.deleteUsers(i).subscribe((resp: any) => {
+      this.users.splice(i, 1);
+    });
+  }
 }
--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,9 +12,14 @@ export class MainService {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 
-  postUsers() {
-    return this.http.post('https://jsonplaceholder.typicode.com/users', {
-      name: 'John Doe',
-    });
+  deleteUsers(i: any): Observable<{}> {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const deleteUrl = url + '/' + i;
+    return this.http.delete(deleteUrl);
+  }
+
+  public postUsers(user: any): Observable<any> {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    return this.http.post<any>(url, user);
   }
 }
